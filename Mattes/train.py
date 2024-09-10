c8c0217896e11464b9b45e21ab7b9ca40e7be870
mawa00001 <mattes.warning@gmx.de>
2024-09-10 12:11:12 +0200

    model but not working

--- Mattes/train.py
+++ Mattes/train.py
@@ -2,61 +2,95 @@
 import os
 import torch
 import argparse
+import sys
+myDir = os.getcwd()
+sys.path.append(myDir)
 
+from pathlib import Path
+path = Path(myDir)
+a=str(path.parent.absolute())
+
+sys.path.append(a)
+
+import torch.nn as nn
 import numpy as np
 import torch.optim as optim
 import matplotlib.pyplot as plt
 import torchvision.transforms as transforms
 
-from model.Model import Model
+from model.Model import Model, Model2
 from dataset.Dataset import MedMNIST2D
 from torch.utils.data import DataLoader
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 def train(model, optimizer, train_loader, val_loader, args):
 
-    criterion = nn.BCEWithLogitsLoss()
+    weight = torch.tensor([4.39, 2.78, 1.3, 12.5, 1.28, 0.21, 10], device=device)
+    criterion = nn.CrossEntropyLoss(weight=weight)# weight=
 
     best_eval_loss = 9999
 
     for epoch in range(args.epochs):
         epoch_loss = 0
         model.train()
-
+        
+        i = 0
         for imgs, canny_imgs, targets in train_loader:
-
+            imgs = imgs.to(device)
+            canny_imgs = canny_imgs.to(device)
+            targets = targets.to(device)
+            
+            
             optimizer.zero_grad()
-
-            outputs = model(imgs, canny_imgs)
+            outputs = model(imgs)
+            #outputs = model(imgs, canny_imgs)
             loss = criterion(outputs, targets)
 
             loss.backward()
             optimizer.step()
+            i+=1
+            epoch_loss += loss
+            
+            break
+            
+            #if i == 5:
+            #    print(outputs, targets)
 
-            avg_loss = epoch_loss / (i + 1)
-            wandb.log({"train-loss": avg_loss})
+        avg_loss = epoch_loss / (i + 1)
+            #wandb.log({"train-loss": avg_loss})
+        print("Train_loss:", avg_loss.item())
 
-            if epoch % args.eval_every == 0:
+        if epoch % args.eval_every == 0:
                 eval_loss = evaluation_loop(model, val_loader)
-                wandb.log({"eval-loss": eval_loss})
+                print("eval_loss:", eval_loss.item())
+                #wandb.log({"eval-loss": eval_loss})
 
-                if eval_loss <= best_eval_loss:
+        if eval_loss <= best_eval_loss:
                     best_eval_loss = eval_loss
                     save_checkpoint(model)
 
 
 def evaluation_loop(model, loader):
     model.eval()
-    criterion = nn.BCEWithLogitsLoss()
+    criterion = nn.CrossEntropyLoss()
 
     running_loss = 0
 
     with torch.no_grad():
+        i = 0
         for imgs, canny_imgs, targets in loader:
+            
+            imgs = imgs.to(device)
+            canny_imgs = canny_imgs.to(device)
+            targets = targets.to(device)
 
-            outputs = model(imgs, canny_imgs)
+            outputs = model(imgs)
+            #outputs = model(imgs, canny_imgs)
             loss = criterion(outputs, targets)
 
             running_loss += loss
+            i += 1
 
         avg_loss = running_loss / (i + 1)
 
@@ -78,13 +112,13 @@ def main(args):
     val_dataset = MedMNIST2D(data_path=args.data_path, split='val', transform=data_transform)
     test_dataset = MedMNIST2D(data_path=args.data_path, split='test', transform=data_transform)
 
-    train_loader = DataLoader(train_dataset)
-    val_loader = DataLoader(val_dataset)
+    train_loader = DataLoader(train_dataset, batch_size=1)
+    val_loader = DataLoader(val_dataset, batch_size=64)
     test_loader = DataLoader(test_dataset)
 
-
-    optimizer = optim.adam()
-    model = Model()
+    model = Model2().to(device)
+    optimizer = optim.Adam(model.parameters())
+    
 
     train(model, optimizer, train_loader, val_loader, args)
 
@@ -92,7 +126,9 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument()
+    parser.add_argument("--data_path", default="data/dermamnist.npz")
+    parser.add_argument("--epochs", default=200)
+    parser.add_argument("--eval_every", default=10)
     args = parser.parse_args()
 
     main(args)
